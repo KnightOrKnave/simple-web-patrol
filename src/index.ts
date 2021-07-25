5e05c3e5ca0669095273a060107c18662e29aa44
mainHome <a11dc094@dhw.ac.jp>
2021-07-25 20:38:32 +0900

    handler

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import { BlowserSiteAccesser } from './siteAccesser';
 import { patrolJob } from './main';
 import { ConsoleLogger } from './accessLogger';
 
-async function main(){
-  const rd=new TxtFileReader("../data/input.txt");
+async function handler(){
+  const rd=new TxtFileReader(`${__dirname}/../data/input.txt`);
   const ac = new BlowserSiteAccesser();
   const lg = new ConsoleLogger();
 
-  cron.schedule('0,0,*,*,*,*',async ()=>{
+  cron.schedule('0 * * * *', async ()=>{
     patrolJob(rd,ac,lg);
   });
 }
 
-main();
+handler();
