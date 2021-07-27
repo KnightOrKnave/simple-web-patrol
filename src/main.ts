5026b6c2949ff395938b8fc5e3aed3a14ea680b9
mainHome <a11dc094@dhw.ac.jp>
2021-07-27 22:42:02 +0900

    f

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { logger } from "./accessLogger";
 import { siteAccesser } from "./siteAccesser";
 import { urlListReader } from "./urlListReader";
 
+const sleep = (waitMillsec:number) => new Promise<void>(resolve => {
+  setTimeout(() => {
+    resolve()
+  }, waitMillsec)
+})
+
 export async function patrolJob(reader: urlListReader, accesser: siteAccesser, logger: logger) {
   logger.writeLine("start patrol");
   const list = await reader.readAll();
   for (let u of list) {
-    const res = await accesser.access(u);
-    logger.writeLine(JSON.stringify(res));
+    const res =  await Promise.all([accesser.access(u),sleep(1*60*1000)]);
+    logger.writeLine(JSON.stringify(res[0]));
   }
   logger.writeLine("end patrol");
 }
