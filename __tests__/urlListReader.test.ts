d21ebda7afbd327eb25f76e0b53aec209cde40c7
mainHome <a11dc094@dhw.ac.jp>
2021-07-31 11:11:29 +0900

    for test

--- __tests__/urlListReader.test.ts
+++ __tests__/urlListReader.test.ts
@@ -1,21 +1,57 @@
-import {TxtFileReader, WebsiteLinkReader} from "../src/urlListReader";
+import http from "http";
+import { TxtFileReader, WebsiteLinkReader } from "../src/urlListReader";
 
-test('正常系_reader', async ()=>{
+function generateHtmlLinkList(linkList: string[]): string {
+  const aTags = linkList.map((a) => `<a href="${a}>link here</a>`);
+  const tg = aTags.join("\n");
+  const resHtml = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='utf-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <title>Page Title</title>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+  <script src='main.js'></script>
+</head>
+<body>
+${tg}
+</body>
+</html>`;
+  return resHtml;
+}
+
+test("正常系_reader", async () => {
   const rd = new TxtFileReader(`${__dirname}/data/正常なURLリスト.txt`);
   const actual = await rd.readAll();
-  const exp = ['https://example.com','https://google.com','https://yahoo.co.jp'];
+  const exp = [
+    "https://example.com",
+    "https://google.com",
+    "https://yahoo.co.jp",
+  ];
   expect(actual).toEqual(exp);
 });
 
-test('異常系_ファイルがない', async ()=>{
+test("異常系_ファイルがない", async () => {
   const rd = new TxtFileReader(`${__dirname}/data/nofile`);
   const actual = await rd.readAll();
-  const exp:string[] = [];
+  const exp: string[] = [];
   expect(actual).toEqual(exp);
 });
 
-test('正常_websiteReader_read',async()=>{
-  const rd = new WebsiteLinkReader('https://io3000.com/');
-  const actual = await rd.readAll();
-  console.log(actual);
-},15*1000)
+test(
+  "正常_websiteReader_read",
+  async () => {
+    const server = http
+      .createServer((req, res) => {
+        res.end;
+      })
+      .listen(10045);
+
+    const rd = new WebsiteLinkReader("https://localhost:10045");
+    const actual = await rd.readAll();
+    console.log(actual);
+  },
+  15 * 1000
+);
