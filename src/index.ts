d15f9956d4598c486b433c5b443cc37e312c9e34
mainHome <a11dc094@dhw.ac.jp>
2021-12-18 17:30:25 +0900

    待ち時間を引数化

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,20 @@ import { patrolJob } from "./main";
 import { ConsoleLogger } from "./accessLogger";
 import * as yargs from "yargs";
 
-async function handler(argv:any) {
+async function handler(argv: any) {
   const ac = new BlowserSiteAccesser();
   const lg = new ConsoleLogger();
   if (argv._[0] === "patrol") {
     const rd = new TxtFileReader(`${__dirname}/../data/input.txt`);
     cron.schedule("0 * * * *", async () => {
-      patrolJob(rd, ac, lg);
+      patrolJob(rd, ac, lg, argv.interval);
     });
-  } else if (argv._[0] === "lateral" && argv['targetUrl']) {
-    const rd = new WebsiteLinkReader(argv['targetUrl']);
-    patrolJob(rd, ac, lg);
+  } else if (argv._[0] === "lateral" && argv["targetUrl"]) {
+    const rd = new WebsiteLinkReader(argv["targetUrl"]);
+    patrolJob(rd, ac, lg, argv.interval);
   } else if (argv._[0] === "oneshot") {
     const rd = new TxtFileReader(`${__dirname}/../data/input.txt`);
-    patrolJob(rd, ac, lg);
+    patrolJob(rd, ac, lg, argv.interval);
   } else {
     console.log(`invalid paramaters`);
   }
@@ -39,25 +39,25 @@ const argv = yargs
   .demandCommand(1)
   .option("depth", {
     alias: "d",
-    type:'string',
+    type: "string",
     description: "depth of get link",
     demandOption: false,
     default: 1,
   })
   .option("interval", {
     alias: "i",
-    type:'number',
+    type: "number",
     description: "access interval seconds",
     demandOption: false,
     default: 60,
-    number:true,
-  }).
-  check((argv)=>{
-    if(argv.depth < 1){
-      return `--depsh should not less than 0`
+    number: true,
+  })
+  .check((argv) => {
+    if (argv.depth < 1) {
+      return `--depsh should not less than 0`;
     }
-    if(argv.interval < 1){
-      return `--interval should not less than 0`
+    if (argv.interval < 1) {
+      return `--interval should not less than 0`;
     }
     return true;
   })
--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,26 @@ import { logger } from "./accessLogger";
 import { siteAccesser } from "./siteAccesser";
 import { urlListReader } from "./urlListReader";
 
-const sleep = (waitMillsec:number) => new Promise<void>(resolve => {
-  setTimeout(() => {
-    resolve()
-  }, waitMillsec)
-})
+const sleep = (waitMillsec: number) =>
+  new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, waitMillsec);
+  });
 
-export async function patrolJob(reader: urlListReader, accesser: siteAccesser, logger: logger) {
+export async function patrolJob(
+  reader: urlListReader,
+  accesser: siteAccesser,
+  logger: logger,
+  intervalSec: number
+) {
   logger.writeLine("start patrol");
   const list = await reader.readAll();
   for (let u of list) {
-    const res =  await Promise.all([accesser.access(u),sleep(1*60*1000)]);
+    const res = await Promise.all([
+      accesser.access(u),
+      sleep(intervalSec * 1000),
+    ]);
     logger.writeLine(JSON.stringify(res[0]));
   }
   logger.writeLine("end patrol");
